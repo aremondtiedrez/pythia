106718cd3ea746d175a6cd911479cf1ebeb1fd42
Antoine Remond-Tiedrez <a.remondtiedrez@gmail.com>
2026-01-30 10:04:04 -0600

    Created the visualization module.
    Created the visualization module and its first routine,
    the function 'inspect' which displays visually a single image
    trajectory.

---
+++ pythia/__init__.py
@@ -0,0 +1,10 @@
+"""
+PyThia performs video prediction for simple physical systems:
+given initial video frames, what will a video frame in the future look like?
+"""
+
+__version__ = "0.1.0"
+
+from . import visualization
+
+__all__ = ["visualization"]
